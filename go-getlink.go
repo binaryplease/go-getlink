d7e3ca06a8fd1918d6c2eae329a7443969cb6a4c
Pablo <pablo1@mailbox.org>
2017-08-21 13:17:40 +0200

    added base for concurrent crawling

--- go-getlink.go
+++ go-getlink.go
@@ -4,54 +4,154 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/net/html"
+	"net/http"
+	"strings"
 )
 
-type Domain struct {
-	root     string
-	subpages []string
+type Url struct {
+	url string
+	id  int
+}
+
+// Helper function to pull the href attribute from a Token
+func getHref(t html.Token) (ok bool, href string) {
+	// Iterate over all of the Token's attributes until we find an "href"
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	// "bare" return will return the variables (ok, href) as defined in
+	// the function definition
+	return
+}
+
+// Extract all http** links from a given webpage
+func crawl(url string, ch chan string, chFinished chan bool) {
+	resp, err := http.Get(url)
+
+	defer func() {
+		// Notify that we're done after this function
+		chFinished <- true
+	}()
+
+	if err != nil {
+		fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
+		return
+	}
+
+	b := resp.Body
+	defer b.Close() // close Body when the function returns
+
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			// Check if the token is an <a> tag
+			isAnchor := t.Data == "a"
+			if !isAnchor {
+				continue
+			}
+
+			// Extract the href value, if there is one
+			ok, url := getHref(t)
+			if !ok {
+				continue
+			}
+
+			// Make sure the url begines in http**
+			hasProto := strings.Index(url, "http") == 0
+			if hasProto {
+				ch <- url
+			}
+		}
+	}
+}
+
+func save_links(urls []string, rootId int) {
+
 }
 
 func main() {
-	domain := get_domain()
-	get_links(&domain)
-	show(domain)
+
+	batchsize := 5
+
+	for {
+		foundUrls := make(map[string]bool)
+		seedUrls := get_urls(batchsize)
+
+		if len(seedUrls) != batchsize {
+			break
+		}
+
+		// Channels
+		chUrls := make(chan string)
+		chFinished := make(chan bool)
+
+		// Kick off the crawl process (concurrently)
+		for _, url := range seedUrls {
+			go crawl(url.url, chUrls, chFinished)
+		}
+
+		// Subscribe to both channels
+		for c := 0; c < len(seedUrls); {
+			select {
+			case url := <-chUrls:
+				foundUrls[url] = true
+			case <-chFinished:
+				c++
+			}
+		}
+
+		// We're done! Print the results...
+
+		fmt.Println("\nFound", len(foundUrls), "unique urls:")
+
+		for url, _ := range foundUrls {
+			fmt.Println(" - " + url)
+		}
+
+		close(chUrls)
+
+		mark_urls_done(seedUrls)
+	}
 }
 
-func show(domain Domain) {
-	fmt.Print("Domain root: ")
-	fmt.Println(domain.root)
-	fmt.Print("Domain's subpages: ")
-	fmt.Println(domain.subpages)
-}
-
-func get_links(domain *Domain) {
+func mark_urls_done(urls []Url) {
 
 }
-func get_domain() Domain {
+
+func get_urls(num int) []Url {
+	var urls []Url
 
 	db, err := sql.Open("mysql", "root:hallo@/gogetlink?charset=utf8")
 	checkErr(err)
-
-	rows, err := db.Query("SELECT * FROM domains where status = 0 limit 1")
+	rows, err := db.Query("SELECT * FROM urls where status = 0 limit " + string(num))
 	checkErr(err)
 
 	var id int
-	var domain string
+	var url string
 	var status int
-	var sub []string
 
 	for rows.Next() {
-
-		err = rows.Scan(&id, &domain, &status)
+		err = rows.Scan(&id, &url, &status)
 		checkErr(err)
-		//fmt.Println(id)
-		//fmt.Println(domain)
-		//fmt.Println(status)
+		urls = append(urls, Url{url, id})
 	}
 
 	db.Close()
-
-	return Domain{domain, sub}
+	return urls
 }
 
 func checkErr(err error) {
